73bda4c935551b771ae979f0ae20dd67378acc2a
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-03-18 22:06:06 -0400

    Created authentication system and it's urls and forms

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,9 @@
+from email import message
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="/accounts/loogin")
+def index(request):
+  message = "Love DJANGO!!"
+  return render (request , 'neighbour/index.html',{"message":message})
--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'neighbour',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,10 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'neighbourhood',
+        'USER': 'moringa',
+        'PASSWORD': 'access',
     }
 }
 
@@ -116,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR,"static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
